e85f8aadef17e6b53502de53e88344feb5b78c12
Jason Maligon <jason.maligon@gmail.com>
2018-04-24 18:33:23 -1000

    added view ticket stuff

--- app/imports/ui/pages/ViewTicket.jsx
+++ app/imports/ui/pages/ViewTicket.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Table, Grid, Loader, Header, Segment, Button, Icon } from 'semantic-ui-react';
+import { Table, Grid, Loader, Header, Segment, Button, Icon, Feed } from 'semantic-ui-react';
 import { Tickets, TicketSchema } from '/imports/api/ticket/ticket';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import AddNote from '/imports/ui/components/AddNote';
+import Note from '/imports/ui/components/Note';
+import { Roles } from 'meteor/alanning:roles';
+import { Notes } from '/imports/api/note/note';
 
 /** Renders the Page for editing a single document. */
 class ViewTicket extends React.Component {
@@ -26,19 +29,22 @@ class ViewTicket extends React.Component {
               <Table.Header>
                 <Table.Row>
                   <Table.HeaderCell>
-                    Status
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>
-                    Ticket Name
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>
                     Building
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    Floor
                   </Table.HeaderCell>
                   <Table.HeaderCell>
                     Room
                   </Table.HeaderCell>
                   <Table.HeaderCell>
-                    Issued By
+                    Priority
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    Description
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    Ticket Status
                   </Table.HeaderCell>
                   <Table.HeaderCell>
                     Date Created
@@ -49,20 +55,35 @@ class ViewTicket extends React.Component {
                 </Table.Row>
               </Table.Header>
               <Table.Body>
-                <Table.Row>
-                  <Table.Cell negative selectable>Unresolved</Table.Cell>
-                  <Table.Cell>Broken door handle</Table.Cell>
-                  <Table.Cell>POST</Table.Cell>
-                  <Table.Cell>1st floor Mens bathroom</Table.Cell>
-                  <Table.Cell selectable>John (john@hawaii.edu)</Table.Cell>
-                  <Table.Cell>2018-02-07 12:05</Table.Cell>
-                  <Table.Cell negative>No Update</Table.Cell>
-                </Table.Row>
+                <Table.Cell>
+                  {this.props.ticket.building}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.floor}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.room}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.priority}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.description}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.status}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.createdOn.toLocaleDateString('en-US')}
+                </Table.Cell>
+                <Table.Cell>
+                  {this.props.ticket.updatedOn.toLocaleDateString('en-US')}
+                </Table.Cell>
               </Table.Body>
             </Table>
             <Segment>
               <Table celled fixed>
-                <Table.Row>Description</Table.Row>
+                Description
                 <Table.Row>
                   Some long description of the problem
                 </Table.Row>
@@ -73,8 +94,20 @@ class ViewTicket extends React.Component {
               <Button floated='right'><Icon className='exclamation'/></Button>
             </Segment>
             <Segment>
-              <AddNote/>
+              Comments
+              <Feed>
+                {this.props.notes.map((note, index) => <Note key={index} note={note.contactId === this.props.ticket._id}/>)}
+              </Feed>
             </Segment>
+            <Segment>
+              <AddNote owner={this.props.ticket.owner} contactId={this.props.ticket._id}/>
+            </Segment>
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+                <Segment>
+                  <Button>Delete</Button>
+                  <Button>Update</Button>
+                </Segment>
+            ) : ''}
           </Grid.Column>
         </Grid>
     );
@@ -86,8 +119,8 @@ ViewTicket.propTypes = {
   doc: PropTypes.object,
   model: PropTypes.object,
   ready: PropTypes.bool.isRequired,
+  ticket: PropTypes.object.isRequired,
   notes: PropTypes.array.isRequired,
-  contact: PropTypes.object.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
@@ -98,6 +131,8 @@ export default withTracker(({ match }) => {
   const subscription = Meteor.subscribe('Tickets');
   return {
     doc: Tickets.findOne(documentId),
+    ticket: Tickets.findOne(documentId),
     ready: subscription.ready(),
+    notes: Notes.find({}).fetch(),
   };
 })(ViewTicket);
