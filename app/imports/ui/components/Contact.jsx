d14c8c94155e322d043cc7e860ad40c5c3dc85c9
Unknown <mkurihar@hawaii.edu>
2018-04-26 02:15:10 -1000

    Delete prototype added
    Delete Button added to TicketAdmin and is functional

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,39 +1,67 @@
 import React from 'react';
-import { Card, Image, Feed } from 'semantic-ui-react';
+import { Button, Card, Feed, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Contacts } from '/imports/api/contact/contact';
 import Note from '/imports/ui/components/Note';
 import AddNote from '/imports/ui/components/AddNote';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+/** Renders a single row in the List Contacts table. See pages/ListContacts.jsx. */
 class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  /** Notify the user of the results of the submit. If successful, clear the form. */
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Delete succeeded' });
+    }
+  }
+
+  /** On submit, insert the data. */
+  onClick() {
+    /* eslint-disable-next-line */
+    if (confirm('Are Really Really Sure You Want To Delete This Contact???')) {
+      Contacts.remove(this.props.contact._id, this.deleteCallback);
+    }
+  }
+
   render() {
     return (
-      <Card centered>
-        <Card.Content>
-          <Image floated='right' size='mini' src={this.props.contact.image} />
-          <Card.Header>
-            {this.props.contact.firstName} {this.props.contact.lastName}
-          </Card.Header>
-          <Card.Meta>
-            {this.props.contact.address}
-          </Card.Meta>
-          <Card.Description>
-            {this.props.contact.description}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
-        </Card.Content>
-        <Card.Content extra>
-          <Feed>
-            {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
-          </Feed>
-        </Card.Content>
-        <Card.Content extra>
-          <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
-        </Card.Content>
-      </Card>
+        <Card centered>
+          <Card.Content>
+            <Image floated='right' size='mini' src={this.props.contact.image} />
+            <Card.Header>
+              {this.props.contact.firstName} {this.props.contact.lastName}
+            </Card.Header>
+            <Card.Meta>
+              {this.props.contact.address}
+            </Card.Meta>
+            <Card.Description>
+              {this.props.contact.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Feed>
+              {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+            </Feed>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
+          </Card.Content>
+          <Card.Content extra>
+            <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+          <Card.Content>
+            <Button basic onClick={this.onClick}>Delete</Button>
+          </Card.Content>
+        </Card>
     );
   }
 }
