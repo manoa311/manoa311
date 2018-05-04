b49d73967eda7f5ebe854fdb9a9d55ee24786cc5
Unknown <mkurihar@hawaii.edu>
2018-05-04 11:18:43 -1000

    refined signin
    Upon Sigin:  Admin goes to admin page and user goes to mytickets page

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -49,16 +49,21 @@ export default class Signin extends React.Component {
     // const isAdministrator = this.state.isAdmin;
     const defaultLanding = { pathname: '/' };
     const defaultAdmin = { pathname: '/admin-ticket' };
+    const defaultMyTicket = { pathname: '/my-tickets' };
     const userEmail = this.state.email;
-    function goToAfterSignIn(e) {
-      if (e === 'admin@foo.com') {
-        return defaultAdmin;
+    const redirectStatus = this.state.redirectToReferer;
+    function goToAfterSignIn(email, redirect) {
+      if (redirect === true) {
+        if (email === 'admin@foo.com') {
+          return defaultAdmin;
+        }
+        return defaultMyTicket;
       }
-      return defaultLanding;
+        return defaultLanding;
     }
 
     // const { from } = this.props.location.state || { from: { pathname: '/' } };
-    const { from } = this.props.location.state || { from: goToAfterSignIn(userEmail) };
+    const { from } = this.props.location.state || { from: goToAfterSignIn(userEmail, redirectStatus) };
     // if correct authentication, redirect to page instead of login screen
     if (this.state.redirectToReferer) {
       return <Redirect to={from}/>;
